2de2081a4b42f3f39e13d89edaa12de0a79af4d6
Ivan <95573815+iscrg@users.noreply.github.com>
2023-12-04 22:49:48 +0700

    Edit acceptCommand.

--- main.py
+++ main.py
@@ -1,39 +1,70 @@
 import os
 
-MENU = '1. Просмотр каталога \n2. На уровень вверх \n3. На уровень вниз \n4. Количество файлов и каталогов \n5. Размер текущего каталога (в байтах) \n6. Поиск файла \n7. Выход из программы \nВыбери пункт меню'
+
+print(list(os.scandir()))
+
+with open('cui') as file:
+    print(file.read())
+import os
+
+MENU = ' \n' \
+       '1. Просмотр каталога \n' \
+        '2. На уровень вверх \n' \
+        '3. На уровень вниз \n' \
+        '4. Количество файлов и каталогов \n' \
+        '5. Размер текущего каталога (в байтах) \n' \
+        '6. Поиск файла \n' \
+        '7. Выход из программы \n'
 
 QUIT = '7'
 
+
 def acceptCommand():
-    pass
+    COMMANDS = [str(x) for x in range(1, 8)]
+
+    command = input('Выберите пункт меню:')
+
+    if command in COMMANDS:
+        return command
+    return None
+
 
 def runCommand(command):
     pass
 
+
 def moveUp():
     pass
+
 
 def moveDown(currentDir):
     pass
 
+
 def countFiles(path):
     pass
+
 
 def countBytes(path):
     pass
 
+
 def findFiles(target, path):
     pass
 
+
 def main(): 
     while True: 
-        print (os.getcwd()) 
-        print (MENU) 
-        command = acceptCommand() 
-        runCommand(command) 
+        print(os.getcwd())
+        print(MENU)
+
+        command = acceptCommand()
+        runCommand(command)
+
         if command == QUIT: 
-            print ('Работа программы завершена.') 
+            print('Работа программы завершена.')
             break
-        
+
+
 if __name__ == '__main__':
     main()
