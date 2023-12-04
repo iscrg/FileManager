8969e6e7160199810bcd0e6f4a08b655c7007bed
dmitryfedyakin <d.fedyakin@g.nsu.ru>
2023-12-04 22:28:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ import os
 
 MENU = '1. Просмотр каталога \n2. На уровень вверх \n3. На уровень вниз \n4. Количество файлов и каталогов \n5. Размер текущего каталога (в байтах) \n6. Поиск файла \n7. Выход из программы \nВыбери пункт меню'
 
+QUIT = '7'
+
+def acceptCommand():
+    pass
+
+def runCommand(command):
+    pass
+
+def moveUp():
+    pass
+
+def moveDown(currentDir):
+    pass
+
+def countFiles(path):
+    pass
+
+def countBytes(path):
+    pass
+
+def findFiles(target, path):
+    pass
+
 def main(): 
     while True: 
         print (os.getcwd()) 
